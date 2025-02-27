e64b5d64c6f22c626d9ce5b9f7dc4fd448a58ad6
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-02-27 17:47:40 -0300

    Add abstract MessageBroker interface for message brokering

---
+++ src/infrastructure/interfaces/message_broker.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+#   subscribe(
+#     queue: string,
+#     onMessage: (msg: Message, ack: () => void, nack: (requeue?: boolean) => void) => Promise<void>,
+#     onError: (error: Error, msg: Message, ack: () => void, nack: (requeue?: boolean) => void) => Promise<void>
+#   ): Promise<void>
+#   ack(msg: Message): Promise<void>
+#   nack(msg: Message, requeue: boolean): Promise<void>
+class MessageBroker(ABC):
+    
+    @abstractmethod
+    async def connection(self, url: str):
+        pass
+
+    @abstractmethod
+    async def publish(self, queue: str, message: dict) -> None:
+        pass
+
+    @abstractmethod
+    async def subscribe(self, queue: str, on_message ) -> None:
+        pass
+        
