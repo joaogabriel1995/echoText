40e246c32b70bebc9166d73823e75cca299bcf8c
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-01 15:01:55 -0300

    Enhance WhisperTranscriber to format transcription segments with start and end times

--- src/infrastructure/whisper.py
+++ src/infrastructure/whisper.py
@@ -9,8 +9,21 @@ class WhisperTranscriber(Transcriber):
     def transcribe(self, audio_path: str) -> str:
         try:
             
-            result = self.model.transcribe(audio_path, verbose=True)
-        
-            return result["text"]
+            result = self.model.transcribe(audio_path)
+            segmentos = []
+            for segment in result.get('segments', []):
+                start_time = segment.get('start', 0)
+                end_time = segment.get('end', 0)
+                text = segment.get('text', "")
+                
+                # Formata os dados do segmento e adiciona à lista
+                segmentos.append(
+                    f"Texto: {text}\nInício: {start_time:.2f} segundos\nFim: {end_time:.2f} segundos\n"
+                )
+            
+            # Une todos os segmentos em uma única string
+            content = "\n".join(segmentos)
+            
+            return content
         except Exception as e:
             print(e)
