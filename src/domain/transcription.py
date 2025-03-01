d1caeaeb557059505f50c5ebb8c73017638c5d2e
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-01 15:01:44 -0300

    Add audio_path attribute and to_dict method to Transcription class

--- src/domain/transcription.py
+++ src/domain/transcription.py
@@ -3,4 +3,14 @@ from dataclasses import dataclass
 @dataclass
 class Transcription:
     text: str
-    url: str
+    audio_path: str
+
+    def __init__(self, text, audio_path):
+        self.text = text
+        self.audio_path = audio_path
+
+    def to_dict(self):
+        return {
+            "text": self.text,
+            "audio_path": self.audio_path
+    }
