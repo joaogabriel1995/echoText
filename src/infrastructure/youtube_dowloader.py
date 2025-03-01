6718a4f3c957ab10290c1f606d9d73a80a5f60b2
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-01 15:01:50 -0300

    Refactor YoutubeDownloader to utilize Download class for audio file naming and initialization

--- src/infrastructure/youtube_dowloader.py
+++ src/infrastructure/youtube_dowloader.py
@@ -1,10 +1,12 @@
 import yt_dlp
 from config.settings import settings 
-
+from domain.download import Download
 class YoutubeDownloader:
     def download_audio(self, url: str) -> str:
-        output_path = f"{settings.TEMP_DIR}/{url.split('=')[-1]}.mp3"
-        print("output_path", output_path)
+        output_path = Download.generateName(settings.TEMP_DIR,url)
+        dowload_obj  = Download(url, output_path) 
+
+        print("output_path", dowload_obj)
         ydl_opts = {
             'format': 'bestaudio/best',
             'outtmpl': output_path,
@@ -16,7 +18,7 @@ class YoutubeDownloader:
             'http_headers': {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
             },
-            'verbose': True,  # Enable detailed output for debugging
+            
             
         }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
