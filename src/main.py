3921323c91191c8a9ca266098aa5da360c98bd56
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-01 15:01:06 -0300

    Integrate FetchAndTranscribeUseCase for combined audio downloading and transcription

--- src/main.py
+++ src/main.py
@@ -10,19 +10,35 @@ from infrastructure.whisper import WhisperTranscriber
 
 
 from application.use_cases.transcriber import TranscribeUseCase
+from application.use_cases.downloader import DownloadAudioUseCase
+from application.dtos.download_and_transcribe_dto import FetchAndTranscribeDTO
+from application.use_cases.fetch_and_transcribe import FetchAndTranscribeUseCase
 
-dowloader = YoutubeDownloader()
+rabbitmq = RabbitMQ()
+
+
+youtubeDownloader = YoutubeDownloader()
 whisperTranscriber = WhisperTranscriber()
-transcribe = TranscribeUseCase(dowloader, whisperTranscriber, RabbitMQ)
+transcribe = TranscribeUseCase(whisperTranscriber)
+download = DownloadAudioUseCase(youtubeDownloader)
 
+fetch_transcriber = FetchAndTranscribeUseCase(download, transcribe, rabbitmq)
 
 async def process_transcription(message: aio_pika.IncomingMessage):
     async with message.process():  # Confirma o processamento automaticamente
         raw_body = message.body  # Bytes
+        
         body_str = raw_body.decode()  # Converte para string
         data = json.loads(body_str)  # Converte JSON para dicionário
-        print(f"Mensagem recebida: {data}")
-        await transcribe.execute(data["url"])
+        dataObj = FetchAndTranscribeDTO(
+            language=data.get("language"),
+            status=data.get("status"),
+            url=data.get("url"),
+            trasncriberType=data.get("trasncriberType"),
+            userId=data.get("userId")
+        )
+
+        await fetch_transcriber.execute(dataObj)
 
 async def main():
 
