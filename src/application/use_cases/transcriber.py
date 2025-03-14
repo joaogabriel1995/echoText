0325240711c43aa5910fb48ddb1e90c21dd68960
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-14 11:42:03 -0300

    Fix transcribe method call in TranscribeUseCase to await asynchronous execution

--- src/application/use_cases/transcriber.py
+++ src/application/use_cases/transcriber.py
@@ -1,7 +1,8 @@
-from infrastructure.interfaces.downloader import  Downloader
+from infrastructure.interfaces.downloader import Downloader
 from infrastructure.interfaces.transcriber import Transcriber
 from infrastructure.interfaces.message_broker import MessageBroker
 from domain.transcription import Transcription
+
 
 class TranscribeUseCase:
     def __init__(self, transcriber: Transcriber):
@@ -10,8 +11,7 @@ class TranscribeUseCase:
     async def execute(self, audio_path: str) -> Transcription:
         """Interface principal, publica a URL na fila."""
         # await self.publish_url(url)
-    
-        text = self.transcriber.transcribe(f"{audio_path}.mp3")
 
+        text = await self.transcriber.transcribe(f"{audio_path}.mp3")
 
         return Transcription(text=text, audio_path=audio_path)
