2951bf2a9fbc654a636fbf506cb1f0a64c87d144
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-14 11:41:53 -0300

    Refactor downloader.py to improve code formatting and remove unnecessary whitespace

--- src/application/use_cases/downloader.py
+++ src/application/use_cases/downloader.py
@@ -1,18 +1,16 @@
-from infrastructure.interfaces.downloader import  Downloader
+from infrastructure.interfaces.downloader import Downloader
 from infrastructure.interfaces.transcriber import Transcriber
 from infrastructure.interfaces.message_broker import MessageBroker
 from domain.download import Download
+
 
 class DownloadAudioUseCase:
     def __init__(self, downloader: Downloader):
         self.downloader = downloader
 
-
     async def execute(self, url: str) -> Download:
-
 
         output_path = self.downloader.download_audio(url)
         # path = f"{output_path}.mp3"
-        
 
         return Download(url, output_path)
