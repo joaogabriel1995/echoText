633b5bb8ee98ae1dcb3a579e9e1b50dbe6cd758d
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-14 11:42:00 -0300

    Refactor FetchAndTranscribeUseCase for improved readability and code formatting

--- src/application/use_cases/fetch_and_transcribe.py
+++ src/application/use_cases/fetch_and_transcribe.py
@@ -1,21 +1,26 @@
 import json
-from .downloader import  DownloadAudioUseCase
-from .transcriber import TranscribeUseCase 
+from .downloader import DownloadAudioUseCase
+from .transcriber import TranscribeUseCase
 from ..dtos.download_and_transcribe_dto import FetchAndTranscribeDTO
 from infrastructure.interfaces.message_broker import MessageBroker
 
+
 class FetchAndTranscribeUseCase:
 
-    def __init__(self, downloadAudioUseCase: DownloadAudioUseCase, transcribeUseCase: TranscribeUseCase, broker: MessageBroker):            
+    def __init__(
+        self,
+        downloadAudioUseCase: DownloadAudioUseCase,
+        transcribeUseCase: TranscribeUseCase,
+        broker: MessageBroker,
+    ):
         self.downloadAudioUseCase = downloadAudioUseCase
         self.transcribeUseCase = transcribeUseCase
         self.rabbitmq = broker
 
-
     async def execute(self, data: FetchAndTranscribeDTO):
 
         output = await self.downloadAudioUseCase.execute(data.url)
-        transcription  =await self.transcribeUseCase.execute(output.path)
+        transcription = await self.transcribeUseCase.execute(output.path)
 
         # print("transcription transcription transcription", transcription)
         data = data.to_dict()
@@ -27,4 +32,3 @@ class FetchAndTranscribeUseCase:
         dataJson = json.dumps(transcription.to_dict())
 
         await self.rabbitmq.publish("transcription-queue", data)
-
